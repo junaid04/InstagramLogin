4ba0664a8ae07fbe01ed12b1c3ed0825f2972727
Hafiz Muhammad Junaid <junaid.waqar92@gmail.com>
2018-05-24 17:32:54 +0500

    Project Added

---
+++ InstagramLogin/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  InstagramLogin
+//
+//  Created by AJK on 10/17/16.
+//  Copyright © 2016 ajk. All rights reserved.
+//
+
+import UIKit
+
+class Constants {
+    
+    let instagramClientID = ""
+    let instagramSecret = ""
+    let redirectURI = "https://com.ajk.demo/"
+    let authorizationEndPoint = "https://api.instagram.com/oauth/authorize/"
+    let accessTokenEndPoint = "https://api.instagram.com/oauth/access_token/"
+    
+}
